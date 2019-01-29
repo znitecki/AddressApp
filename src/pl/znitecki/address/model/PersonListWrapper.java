261e5ae3e6188a04c81cf7f9b7415f525cd22cae
Zbigniew Nitecki <z.nitecki@gmail.com>
2019-01-29 13:07:55 +0100

    Refactor

---
+++ src/pl/znitecki/address/model/PersonListWrapper.java
@@ -0,0 +1,21 @@
+package pl.znitecki.address.model;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "persons")
+public class PersonListWrapper {
+
+	private List<Person> persons;
+	
+	@XmlElement(name = "person")
+	public List<Person> getPersons() {
+		return persons;
+	}
+	
+	public void setPersons(List<Person> persons) {
+		this.persons = persons;
+	}
+}
