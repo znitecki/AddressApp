14e71b21b075b2827ceb815f1b48c8e62980d0a7
Zbigniew Nitecki <z.nitecki@gmail.com>
2019-01-29 15:27:44 +0100

    BirthdayStatistics added

---
+++ src/pl/znitecki/address/view/BirthdayStatisticsController.java
@@ -0,0 +1,48 @@
+package pl.znitecki.address.view;
+
+import java.text.DateFormatSymbols;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.XYChart;
+import pl.znitecki.address.model.Person;
+
+public class BirthdayStatisticsController {
+	
+	@FXML
+	private BarChart<String, Integer> barChart;
+	
+	@FXML
+	private CategoryAxis xAxis;
+	
+	private ObservableList<String> monthNames = FXCollections.observableArrayList();
+	
+	@FXML
+	private void initialize() {
+		String[] months = DateFormatSymbols.getInstance(Locale.ENGLISH).getMonths();
+		monthNames.addAll(Arrays.asList(months));
+		xAxis.setCategories(monthNames);
+	}
+	
+	public void setPersonData(List<Person> persons) {
+		int[] monthCounter = new int[12];
+		for (Person p : persons) {
+			int month = p.getBirthday().getMonthValue() -1;
+			monthCounter[month]++;
+		}
+		
+		XYChart.Series<String, Integer> series = new XYChart.Series<>();
+		
+		for (int i = 0; i < monthCounter.length; i++) {
+			series.getData().add(new XYChart.Data<>(monthNames.get(i), monthCounter[i]));
+		}
+		
+		barChart.getData().add(series);
+	}
+}
